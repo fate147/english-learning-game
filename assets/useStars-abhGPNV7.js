fc6349a06f141f4f8f6f1987b591ebf0704138c8
fate147 <896595037@qq.com>
2026-07-06 15:53:17 +0800

    Updates

---
+++ assets/useStars-abhGPNV7.js
@@ -0,0 +1,1 @@
+import{H as e,V as t,h as n}from"./index-B19oN0MX.js";var r=e(t(),1);function i(){let e=(0,r.useContext)(n);if(!e)throw Error(`useStars 必须在 StarProvider 内使用`);return e}export{i as t};
